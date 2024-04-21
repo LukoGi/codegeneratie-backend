39bf6f0f5289e69d7e6306eed9551b94a907fce1
Umit3001 <113436232+Umit3001@users.noreply.github.com>
2024-04-21 13:48:50 +0200

    Add files via upload

---
+++ src/test/java/Project_Java/Bank_Application/BankApplicationTests.java
@@ -0,0 +1,13 @@
+package Project_Java.Bank_Application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
