a45af6f77eae1d959219e23406cb8cd701c12745
Gecko <kian.steffes@gmail.com>
2024-05-19 12:43:53 +0200

    atm login without jwt security, also made some modifications which allows us to make actual requests through the frontend

--- src/main/java/spring/group/spring/config/WebSecurityConfiguration.java
+++ src/main/java/spring/group/spring/config/WebSecurityConfiguration.java
@@ -1,21 +1,25 @@
 package spring.group.spring.config;
 
-import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-// TODO User: don't forget to add the necessary annotations
+import java.util.Arrays;
+// TODO: finish jwt and permissions policy configuration see if all the extra stuff I added is necessary
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfiguration {
 
-    // TODO User: finish WebSecurityConfiguration this is just a temporary solution
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors().and()
                 .csrf(c -> c.disable())
                 .authorizeRequests(ar -> ar
                         .anyRequest().permitAll())
@@ -26,5 +30,17 @@ public class WebSecurityConfiguration {
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder(12);
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173/"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
