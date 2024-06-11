6764889189b3ac6bb7d59f9e617e4460bcd37863
Exp <102148117+Explosive12@users.noreply.github.com>
2024-06-11 11:59:23 +0200

    idk at this point pt 3

--- src/test/java/spring/group/spring/CucumberTestRunner.java
+++ src/test/java/spring/group/spring/CucumberTestRunner.java
@@ -1,10 +1,12 @@
+package spring.group.spring;
+
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/resources/cucumber",
+        features = "src/test/resources/cucumber/features",
         plugin = {"pretty"},
         glue = {"spring.group.spring.steps"}
 )
